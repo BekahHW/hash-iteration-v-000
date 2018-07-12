6e1c2c2bd3e0746497b74d71ea2dbde0fcc92084
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 23:19:10 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,6 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each { |key, value| 
+  passengers.each { |key, value| if suite == :suite_a && name.start_with?("A")
+    winner = name end}
 end
